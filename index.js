aa38578984a7e29c587ba5f266d0e586db71b358
Yara1504 <baykenichs@gmail.com>
2024-01-15 17:14:07 +0200

    second push

--- index.js
+++ index.js
@@ -17,21 +17,25 @@ async function invokeAction({ action, id, name, email, phone }) {
     case "list":
       const allContacts = await contacts.listContacts();
       console.table(allContacts);
+
       break;
 
     case "get":
       const getContact = await contacts.getContactById(id);
       console.log(getContact);
+
       break;
 
     case "add":
       const newContact = await contacts.addContact(name, email, phone);
       console.log(newContact);
+
       break;
 
     case "remove":
       const deleteContact = await contacts.removeContact(id);
       console.log(deleteContact);
+      
       break;
 
     default:
